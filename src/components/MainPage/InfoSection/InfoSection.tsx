1f6db98fb18fb8ee15f31f4277b13a75d8eec8d7
Fedor Shapkin <fedorshapkin20090414@gmail.com>
2025-03-27 16:45:25 +0300

    Maded styles to Info component

--- src/components/MainPage/InfoSection/InfoSection.tsx
+++ src/components/MainPage/InfoSection/InfoSection.tsx
@@ -4,88 +4,33 @@ import imgAi from "@/assets/imgAi.png";
 const InfoSection = () => {
     return (
         <div className={`${styles.infoSection}`}>
-            <div style={{
-                width: "327px",
-                height: "29px",
-                gap: "9px"
-            }}>
+            <div  className={"InfoButtons"}>
                 <button className={styles.infoSectionButton}>PDF-format</button>
                 <button className={styles.infoSectionButton}>Web-site</button>
                 <button className={styles.infoSectionButton}>smtng</button>
             </div>
-            <div style={{
-                width: "513px",
-                height: "183.60000610351562px",
-                gap: "10.8px",
-            }}>
-                <div style={{
-                    fontSize: "45px",
-                    lineHeight: "121%",
-                    letterSpacing: "-3%",
-                    fontWeight: "500",
-                    width: "513px",
-                    height: "54px",
-                    paddingTop: "18px"
-                }}>Учитесь быстрее
+            <div className={"BoldText"}>
+                <div className={"BoldH1"}>
+                    Учитесь быстрее
                 </div>
-                <div style={{
-                    width: "513px",
-                    height: "54px",
-                    gap: "23.76px",
-
-                }}>
-                    <div style={{
-                        width: "111px",
-                        height: "53px",
-                        gap: "5px",
-
-                    }}>
+                <div className={"SmallText"}>
+                    <div className={"SmallTextWithImg"}>
                         <img src={imgAi} alt={"sorry,image was lost"}/>
-                        <div style={{
-                            width: "371px",
-                            height: "54px",
-                            fontWeight: "500",
-                            fontSize: "45px",
-                            lineHeight: "121%",
-                            letterSpacing: "-3%",
-                            textAlign: "center",
-                            paddingTop: "11px"
-                        }}>
+                        <div className={"SmallTextAi"}>
                             ИИ-инструменты
                         </div>
                     </div>
 
                 </div>
-                <div style={{
-                    width: "513px",
-                    height: "54px",
-                    fontWeight: "500",
-                    fontSize: "45px",
-                    lineHeight: "121%",
-                    letterSpacing: "-3%",
-                    paddingTop: "11px"
-
-
-                }}> для освоения тем
+                <div className={"SmallTextTopics"}>
+                    для освоения тем
                 </div>
             </div>
-            <div style={{
-                width: "513px",
-                height: "60px",
-                fontFamily: "Inter",
-                fontWeight: "400px",
-                fontSize: "20px",
-                lineHeight: "150%",
-                letterSpacing: "-2%",
-                paddingTop: "18px"
-            }}>
+            <div className={"SmallTextPar"}>
                 Тесты по текстам, заметки по темам для подготовки к экзаменам! Оставьте заявку на ранний
                 доступ
             </div>
-            <div style={{
-                paddingTop: "41px",
-                display: "flex",
-                justifyContent: "center",
+            <div className={"TryButton"} style={{
             }}>
                 <button>Попробовать</button>
             </div>
