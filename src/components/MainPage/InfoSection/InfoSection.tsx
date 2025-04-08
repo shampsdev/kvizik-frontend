abac6e21262cb06f74c5a126f55fc9ddf1bf1c06
Fedor Shapkin <fedorshapkin20090414@gmail.com>
2025-04-08 23:32:19 +0300

    fixed a info-div

--- src/components/MainPage/InfoSection/InfoSection.tsx
+++ src/components/MainPage/InfoSection/InfoSection.tsx
@@ -4,34 +4,34 @@ import imgAi from "@/assets/imgAi.png";
 const InfoSection = () => {
     return (
         <div className={`${styles.infoSection}`}>
-            <div  className={"w-[327px] h-[29px] gap-[9px]"}>
-                <button className={"bg-white text-black w-[109px] h-[29px] gap-[9px] rounded-[36px] border border-black pt-[6px] pr-[13px] pb-[6px] pl-[13px] font-medium text-[14px] leading-[121%] tracking-[-0.03em]"}>PDF-format</button>
-                <button className={"bg-white text-black w-[109px] h-[29px] gap-[9px] rounded-[36px] border border-black pt-[6px] pr-[13px] pb-[6px] pl-[13px] font-medium text-[14px] leading-[121%] tracking-[-0.03em]"}>Web-site</button>
-                <button className={"bg-white text-black w-[109px] h-[29px] gap-[9px] rounded-[36px] border border-black pt-[6px] pr-[13px] pb-[6px] pl-[13px] font-medium text-[14px] leading-[121%] tracking-[-0.03em]"}>smtng</button>
+            <div  className={"w-[327px] h-[29px] gap-[9px] flex"}>
+                <button className={"bg-black text-white w-[109px] h-[29px]  rounded-[36px] border border-[#E1E1E1] pt-[6px] pr-[13px] pb-[6px] pl-[13px] font-medium text-[14px] leading-[121%] tracking-[-0.03em]"}>PDF-format</button>
+                <button className={"bg-black text-white w-[109px] h-[29px]  rounded-[36px] border border-[#E1E1E1] pt-[6px] pr-[13px] pb-[6px] pl-[13px] font-medium text-[14px] leading-[121%] tracking-[-0.03em]"}>Web-site</button>
+                <button className={"bg-black text-white w-[109px] h-[29px]  rounded-[36px] border border-[#E1E1E1] pt-[6px] pr-[13px] pb-[6px] pl-[13px] font-medium text-[14px] leading-[121%] tracking-[-0.03em]"}>smtng</button>
             </div>
-            <div className={" w-[513px] h-[183px] gap-[10px]"}>{/*maybe should use flex*/}
-                <div className={"w-[513px] h-[54px] pt-[18px] text-[45px] leading-[121%] tracking-[-3%] font-medium"}>
+            <div className={" w-[513px] h-[180px] gap-[10px]"}>{/*maybe should use flex*/}
+                <div className={"w-[513px] h-[54px] pt-[18px] text-[28px] leading-[121%] tracking-[-3%] font-medium text-white"}>
                     Учитесь быстрее
                 </div>
-                <div className={"w-[513] h-[54] gap-[23]"}>
-                    <div className={"w-[111] h-[53] gap-[5]"}>
+                <div className={"w-[513] h-[160] gap-[23]"}>
+                    <div className={"w-[111] h-[53] gap-[5] flex"}>
                         <img src={imgAi} alt={"sorry,image was lost"}/>
-                        <div className={"w-[371px] h-[54px] font-medium text-[45px] leading-[121%] tracking-[-3%] text-center pt-[11px]"}>
+                        <div className={"w-[250px] h-[54px]  text-white font-medium text-[28px] leading-[121%] tracking-[-3%] text-center pt-[11px]"}>
                             ИИ-инструменты
                         </div>
                     </div>
 
                 </div>
-                <div className={"w-[513px] h-[54px] font-medium text-[45px] leading-[121%] tracking-[-3%] pt-[11px]"}>
+                <div className={"w-[513px] text-white h-[54px] font-medium text-[28px] leading-[121%] tracking-[-3%] pt-[10px]"}>
                     для освоения тем
                 </div>
             </div>
-            <div className={"w-[513px] h-[60px] font-inter font-normal text-[20px] leading-[150%] tracking-[-2%] pt-[18px]"}>
+            <div className={"w-[513px] text-white h-[100px] font-inter font-normal text-[20px] leading-[150%] tracking-[-2%] pt-[10px]"}>
                 Тесты по текстам, заметки по темам для подготовки к экзаменам! Оставьте заявку на ранний
                 доступ
             </div>
-            <div className={"pt-[41px] flex justify-center"} >
-                <button>Попробовать</button>
+            <div className={"pt-[41px] flex justify-center "} >
+                <button className="text-black bg-white rounded-[18px] w-[495px] h-[45px]">Попробовать</button>
             </div>
         </div>
     );
